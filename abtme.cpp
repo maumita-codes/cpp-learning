00965ebd71354ae4848207bcad02b91a24c7a309
Maumita Roy <maumitaroy26@gmail.com>
2026-08-28 23:01:27 +0530

    Add about me program

---
+++ abtme.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout <<"My name is Maumita"<<endl;
+    cout <<"I am 17 years old"<<endl<<"I am an Engineering student"<<endl<<"My height is 5.6"<<endl;
+    return 0;
+}
