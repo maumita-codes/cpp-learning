7c62ebd56be40884a94958f3f3791962f0c62e10
Maumita Roy <maumitaroy26@gmail.com>
2026-08-28 23:41:15 +0530

    Adding an input program

---
+++ input.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    int age;
+    int favno;
+
+    cout<<"Enter your name: ";
+    cin >> name;
+
+    cout <<"Enter your age: ";
+    cin >>age;
+
+    cout <<"Enter your favourite no: ";
+    cin>>favno;
+
+    cout <<"Hello " << name <<"!"<< endl;
+    cout <<"You are "<< age <<" years old."<< endl;
+    cout <<"Your favourite no is "<< favno;
+
+    return 0;
+   
+}
+
+
+
+
+    
