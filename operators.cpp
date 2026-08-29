2c4a15b31f1d675df0bab86ec267dcc43821fecc
Maumita Roy <maumitaroy26@gmail.com>
2026-08-29 22:35:01 +0530

    learning various operations

---
+++ operators.cpp
@@ -0,0 +1,24 @@
+// Using operators like addition, subtraction, multiplication, division, modulus(remainder)
+
+//(the operations are done for integers.)
+
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+
+cout<<"Enter the first number: ";
+cin>>a;
+
+cout<<"Enter the second number: ";
+cin>>b;
+
+cout <<"Addition: "<< a+b <<endl;
+cout <<"Subtraction: "<< a-b <<endl;
+cout <<"Multiplication: "<< a*b <<endl;
+cout <<"Division"<< a/b <<endl;
+cout <<"Remainder: "<< a%b <<endl;
+
+return 0;
+}
+
