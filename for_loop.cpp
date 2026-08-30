12313bdf7ab7a111915653f6a0bf05ca9fad21c7
Maumita Roy <maumitaroy26@gmail.com>
2026-08-30 22:57:55 +0530

    learning for loops in both orders.

---
+++ for_loop.cpp
@@ -0,0 +1,15 @@
+//learning for loop
+
+#include<iostream>
+using namespace std;
+int main(){
+    //forward
+    for(int i=1; i<=10; i++){
+        cout<<i<<endl;
+    }
+    //reverse 
+    for(int i=10; i>=1; i--){
+        cout<<i<<endl;
+    }
+    
+}
