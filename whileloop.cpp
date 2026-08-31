a720acc9ca61fb657d455532edb938ec3eb40370
Maumita Roy <maumitaroy26@gmail.com>
2026-08-31 21:38:46 +0530

    Learning while loop.

---
+++ whileloop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=5;
+    while(i>=1){
+        cout<<i<<endl;
+        i--;
+    }
+    cout<<"Blast off!"<<endl;
+
+    int p=1;
+    while(p <= 5){
+        cout << p << endl;
+        p++;
+    }
+    cout<<"good job!";
+    return 0;
+}
