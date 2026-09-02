6e246428e01d4eb3a757b32bba7b3858dfa50183
Maumita Roy <maumitaroy26@gmail.com>
2026-09-02 22:24:22 +0530

    Learning arrays with examples.

--- arrays.cpp
+++ arrays.cpp
@@ -3,20 +3,20 @@
 #include <iostream>
 using namespace std;
 int main(){
-    // int numbers[5]={45,67,90,87,12};
-    // for(int i=0; i<5; i++){
-    //     cout<<numbers[i]<<endl;
-    // }
+    int numbers[5]={45,67,90,87,12};
+    for(int i=0; i<5; i++){
+        cout<<numbers[i]<<endl;
+    }
 
-    // int num[4]; //takes numbers form user and prints it
-    // for (int p=0; p<4;p++){
-    //     cout<<"Enter your numbers: "<<endl;
-    //     cin>>num[p];
-    // }
-    // cout<<"your numbers are: "<<endl;
-    // for (int p = 0; p < 4; p++) {
-    // cout << num[p]<< endl;
-    // }
+    int num[4]; //takes numbers form user and prints it
+    for (int p=0; p<4;p++){
+        cout<<"Enter your numbers: "<<endl;
+        cin>>num[p];
+    }
+    cout<<"your numbers are: "<<endl;
+    for (int p = 0; p < 4; p++) {
+    cout << num[p]<< endl;
+    }
 
     int no[4];
     for (int m=0; m<4; m++){
