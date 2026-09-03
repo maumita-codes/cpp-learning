3e01dcf886c52c5d3807508fbae02e81b4713c06
Maumita Roy <maumitaroy26@gmail.com>
2026-09-03 19:00:16 +0530

    Questions solving.

---
+++ quest_array.cpp
@@ -0,0 +1,35 @@
+// solving problems related to arrays
+
+#include <iostream>
+using namespace std;
+int main(){
+    int no[4];
+    for (int m=0; m<4; m++){
+        cout<<"enter your numbers: "<<endl;
+        cin>>no[m];
+    }
+    int sum=0;
+    for(int m=0;m<4;m++){
+        sum=sum+no[m];
+    }
+    double average= (double)sum/4; //to give avg of given numbers by the user.
+    cout<<"the average of the provided numbers is "<<average<<endl;
+
+    int count=0;
+    int ct=0;
+    int num[5];
+    for (int i=0;i<5;i++){
+        cout<<"Enter your numbers: "<<endl;
+        cin>>num[i];
+        if(num[i]%2==0){
+        count=count+1;
+        }                //counts even-odd numbers given
+        else{
+            ct=ct+1;
+        }
+        cout<<"even numbers: "<<count<<endl;
+        cout<<"odd numbers:"<<ct<<endl;
+
+    }
+    return 0;
+}
