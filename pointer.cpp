837761f2a9e4a459fdbbdc8d5cba6c0979c58111
Maumita Roy <maumitaroy26@gmail.com>
2026-09-12 21:55:20 +0530

    addressing.

--- pointer.cpp
+++ pointer.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int y;
-    cout<<"Enter the number: "<<endl;
-    cin>>y;
-    int *ptr=&y;
-    int z;
+    // int y;
+    // cout<<"Enter the number: "<<endl;
+    // cin>>y;
+    // int *ptr=&y;
+    // int z;
+    // cout<<"enter another value: "<<endl;
+    // cin>>z;
+    // *ptr=z;
+    // cout<<"the current data is: "<<*ptr<<endl;
+
+    int x;
+    cout<<"Enter your number: "<<endl;
+    cin>>x;
+    int *ptr=&x;
+    cout<<"Value of x: "<<x<<endl;
+    cout<<"Address of x: "<<&x<<endl;
+    cout << "Address stored in ptr: " << ptr << endl;
+    int h;
     cout<<"enter another value: "<<endl;
-    cin>>z;
-    *ptr=z;
+    cin>>h;
+    *ptr=h;
     cout<<"the current data is: "<<*ptr<<endl;
     return 0;
 }
