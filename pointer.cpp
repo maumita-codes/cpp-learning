def628228c577280e6414abee39b03d2c0eba5f6
Maumita Roy <maumitaroy26@gmail.com>
2026-09-13 20:41:19 +0530

    to double the value of a pointer.

--- pointer.cpp
+++ pointer.cpp
@@ -39,5 +39,29 @@ int main(){
     cout<<"Address stored in ptr: "<<ptr<<endl;
     *ptr= a +10;
     cout<<"The changed value of a is: " <<*ptr<<endl;
+
+//to double the value of the pointer
+    int v;
+    cout<<"Enter the number: "<<endl;
+    cin>>v;
+    int *ptr=&v;
+    cout<<"The value of v: "<<v<<endl;
+    cout<<"Address of v: "<<&v<<endl;
+    cout<<"The address stored in ptr: "<<ptr<<endl;
+    *ptr=v*2;
+    cout<<"The changed value of v is: "<<*ptr<<endl;
+
+
+//to double the value of the pointer
+    int v;
+    cout<<"Enter the number: "<<endl;
+    cin>>v;
+    int *ptr=&v;
+    cout<<"The value of v: "<<v<<endl;
+    cout<<"Address of v: "<<&v<<endl;
+    cout<<"The address stored in ptr: "<<ptr<<endl;
+    *ptr=v*2;
+    cout<<"The changed value of v is: "<<*ptr<<endl;
+    
     return 0;
 }
