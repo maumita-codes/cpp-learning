b2bfd57ce1efcff683b4bcfbac076295ed7ce64c
Maumita Roy <maumitaroy26@gmail.com>
2026-09-16 22:09:24 +0530

    practicing constructor(class,display).

--- oops.cpp
+++ oops.cpp
@@ -2,26 +2,46 @@
 
 #include <iostream>
 using namespace std;
-class Car {
+// class Car {
+// public:
+//     string brand;
+//     int price;
+
+//     Car(string b, int p) {
+//         brand = b;
+//         price = p;
+//     }
+
+//     void display() {
+//         cout << brand << endl;
+//         cout << price << endl;
+//     }
+// };
+class Books{
 public:
-    string brand;
+    string title;
     int price;
 
-    Car(string b, int p) {
-        brand = b;
-        price = p;
+    Books(string i, int m){
+        title=i;
+        price=m;
     }
-
-    void display() {
-        cout << brand << endl;
-        cout << price << endl;
+    void display(){
+        cout<<title<<endl;
+        cout<<price<<endl;
     }
 };
 int main(){
-    Car car1("Audi", 16000000);
-    Car car2("BMW", 15000000);
-    car1.display();
-    car2.display();
+
+    // Car car1("Audi", 16000000);
+    // Car car2("BMW", 15000000);
+    // car1.display();
+    // car2.display();
+
+    Books book1("Harry Potter", 500);
+    Books book2("Atomic Habits", 600);
+    book1.display();
+    book2.display();
 
     return 0;
 }
